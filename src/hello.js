f49f845d5bec873345dd59766f6d6e36605f54f4
aimonansari06 <aimon_ansari06@outlook.com>
2020-12-08 15:29:24 +0500

    Second Commit

--- src/hello.js
+++ src/hello.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function Hello({fname}){
-return <p>Hello from hello.js {fname}</p>
+return <p>Hello from hello.js updated {fname}</p>
 ;
 }
 export default Hello;
